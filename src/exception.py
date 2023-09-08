e8c66a4deb7848919356fc7c94904f00dcbcb641
www.dowtyasriprasanth1729@gmail.com <www.dowtyasriprasanth1729@gmail.com>
2023-09-08 17:12:53 -0500

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys 
+import logging 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(exc_tb.tb_frame.f_code.co_filename,exc_tb.tb_lineno,str(error))
