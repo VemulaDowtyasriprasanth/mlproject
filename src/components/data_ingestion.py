f1cd174518cd76044f1d354e8d3447afaa2c8888
www.dowtyasriprasanth1729@gmail.com <www.dowtyasriprasanth1729@gmail.com>
2023-09-13 19:17:05 -0500

    fuckthis

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os 
 import sys
 import pandas as pd
+# from pickle import OBJ
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.exception import CustomException
 from src.logger import logging
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
@@ -41,4 +43,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     data_ingestion = DataIngestion()
-    data_ingestion.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -9,15 +9,20 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.logger import logging
 from src.exception import CustomException
+from src.utils import save_object
 
 
-@dataclass
+@dataclass      
 class DataTransformationConfig:
-    preprocessor_obj_file_path: str = os.path.join("artifacts", "preprocessor_obj.pkl")
+    preprocessor_obj_file_path: str = os.path.join("artifacts", "preprocessor.pkl")
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
     def get_data_transformer_object(self):
+        """
+        This function is responsible for data transformation
+        
+        """
         try:
             numerical_columns=['writing_score','reading_score']
             categorical_columns=[
@@ -27,6 +32,50 @@ class DataTransformation:
                 "lunch",
                 "test_preparation_course",
             ]
-            
-        except:
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler()),
+                ]
+            )
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler()),
+                ]
+            )
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+            preprocessor = ColumnTransformer(
+                ("num_pipleline", num_pipeline, numerical_columns),
+                ("cat_pipeline", cat_pipeline, categorical_columns),
+            )
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e,sys)
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            logging.info("Read train and test data Completed")
+            logging.info("Obtaining pre processor object")
+            preprocessing_obj=self.get_data_transformer_object()
+            target_column_name="math_score"
+            numerical_columns=['writing_score','reading_score']
+            input_feature_df=train_df.drop(target_column_name,axis=1)
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                object=preprocessing_obj,
+            )
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+
+
             pass
+        except Exception as e :
+            raise CustomException(e,sys)
+        
